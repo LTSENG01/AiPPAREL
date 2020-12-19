93679d262a8697f9363b5d167c1fa0935f114505
Larry Tseng <larry.tseng.1@gmail.com>
2020-12-19 12:45:58 -0500

    Add default case to status checking

--- server/routes/design.js
+++ server/routes/design.js
@@ -15,16 +15,17 @@ router.get('/', function(req, res, next) {
         hash = req.params["hash"]
 
         // regex the hash, toss if fails!
-        let regex = /^[A-Za-z0-9_-]{21}$/
+        const regex = /^[A-Za-z0-9_-]{21}$/
         if (!regex.test(hash)) {
             return res.status(400).send("Invalid hash.")
         }
     } else {
-        return res.status(404).send("Error. Design not found!")
+        return res.status(400).send("Error. Bad Hash!")
     }
 
     // read the JSON file synchronously from filesystem (migrate to DB later)
-    const path = `../status/${hash}.json`
+    const path = `./status/${hash}.json`
+    console.log(path + " " + fs.existsSync(path))
     if (fs.existsSync(path)) {
         let jsonContents = fs.readFileSync(path)
         let designStatus = JSON.parse(jsonContents.toString())
@@ -35,8 +36,9 @@ router.get('/', function(req, res, next) {
                 case IN_PROGRESS:
                     return res.status(202).send("Processing...")
                 case SUCCESS:
-                    return res.status(200).send(designStatus["status"])
+                    return res.status(200).send("Success!")
                 case ERROR:
+                default:
                     return res.status(500).send("Internal Server Error. Unable to Process Your Design.")
             }
         } else {
@@ -44,7 +46,7 @@ router.get('/', function(req, res, next) {
         }
     } else {
         // json file does NOT exist
-        return res.status(404).send("Design Status Not Found.")
+        return res.status(404).send("Error. Design Status is Missing!")
     }
 });
 
