a5531b6f2d6cb51bbd1cb59fe814db17af397bc9
Larry Tseng <larry.tseng.1@gmail.com>
2020-12-19 11:54:08 -0500

    Finish design endpoint

--- server/routes/design.js
+++ server/routes/design.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+const {IN_PROGRESS, ERROR, SUCCESS} = require('../../enums/status.js');
+
 const router = express.Router({ mergeParams : true });
 
 /**
@@ -8,25 +10,40 @@ const router = express.Router({ mergeParams : true });
 router.get('/', function(req, res, next) {
 
     // get the hash from the URL
-    if (req.params && "hash" in req.params) {
-        const hash = req.params["hash"]
+    let hash
+    if (typeof req.params == "object" && "hash" in req.params) {
+        hash = req.params["hash"]
+
+        // regex the hash, toss if fails!
         console.log(hash)
     } else {
-        res.status(404).send("Error. Design not found!")
+        return res.status(404).send("Error. Design not found!")
     }
 
-    // read the JSON file synchronously from filesystem
+    // read the JSON file synchronously from filesystem (migrate to DB later)
+    const path = `../status/${hash}.json`
+    if (fs.existsSync(path)) {
+        let jsonContents = fs.readFileSync(path)
+        let designStatus = JSON.parse(jsonContents.toString())
 
-    // parse the JSON file
-
-    // send back a response
-
-    res.send(req.params)
+        // parse the JSON file
+        if ("status" in designStatus) {
+            switch (parseInt(designStatus["status"])) {
+                case IN_PROGRESS:
+                    return res.status(202).send("Processing...")
+                case SUCCESS:
+                    return res.status(200).send(designStatus["status"])
+                case ERROR:
+                    return res.status(500).send("Internal Server Error. Unable to Process Your Design.")
+            }
+        } else {
+            return res.status(500).send("Internal Server Error. Design Status Unavailable.")
+        }
+    } else {
+        // json file does NOT exist
+        return res.status(404).send("Design Status Not Found.")
+    }
 });
 
-// todo CAN YOU HAVE A GET AND POST?
-router.post('/', function(req, res, next) {
-
-});
 
 module.exports = router;
