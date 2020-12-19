1dd4a6396ba2283d3969408847affda65a56cee6
KelseyMichelle <gregwarek@gmail.com>
2020-12-19 14:16:41 -0500

    updated syling

--- server/routes/design.js
+++ server/routes/design.js
@@ -1,32 +1,29 @@
-const express = require('express');
-const fs = require('fs');
-const router = express.Router({ mergeParams : true });
+const express = require("express");
+const fs = require("fs");
+const router = express.Router({ mergeParams: true });
 
 /**
  * This route returns the status of the process (JSON file with hash)
  */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
+  // get the hash from the URL
+  if (req.params && "hash" in req.params) {
+    const hash = req.params["hash"];
+    console.log(hash);
+  } else {
+    res.status(404).send("Error. Design not found!");
+  }
 
-    // get the hash from the URL
-    if (req.params && "hash" in req.params) {
-        const hash = req.params["hash"]
-        console.log(hash)
-    } else {
-        res.status(404).send("Error. Design not found!")
-    }
+  // read the JSON file synchronously from filesystem
 
-    // read the JSON file synchronously from filesystem
+  // parse the JSON file
 
-    // parse the JSON file
+  // send back a response
 
-    // send back a response
-
-    res.send(req.params)
+  res.send(req.params);
 });
 
 // todo CAN YOU HAVE A GET AND POST?
-router.post('/', function(req, res, next) {
-
-});
+router.post("/", function (req, res, next) {});
 
 module.exports = router;
