e8d1c4f2e4bfa5c8c8233bed0497b40c8ab4b34f
KelseyMichelle <gregwarek@gmail.com>
2020-12-19 01:49:53 -0500

    image download route

--- server/routes/stylize.js
+++ server/routes/stylize.js
@@ -1,32 +1,76 @@
 const express = require('express');
 const { spawn } = require('child_process');
-
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+const shortid = require('shortid')
+const app = express();
 const router = express.Router();
+const {IN_PROGRESS, ERROR, SUCCESS} = require('../../enums/status.js');
+
+app.use(fileUpload({createParentPath: true}));
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan('dev'));
+
+function writeProgress(progress) {
+  fs.writeFile(`./status/${progress.id}.json`, JSON.stringify(progress));
+}
 
 /* GET stylize listing. */
 router.post('/', function(req, res, next) {
-  // grab image link from request body
-      // do we include raw images as well?
+  let id = shortid.generate();
 
-  // preprocess the image
+    // id: String, contentImage: String, styleImages: String[]
+  let images = {id: id, contentImage: "", styleImages: []};
+  let progress = { status: IN_PROGRESS, id: id };
+  
+  try {
+    // if no files attached, return error/failed
+      if(!req.files) {
+          res.send({
+              status: false,
+              message: 'no images included'
+          });
+      } else {
+          let data = [];
+          Object.keys(req.files.photos).forEach((key) => {
+              let photo = req.files.photos[key];
+              photo.mv('./uploads/' + photos.name);
+              data.push({name: photo.name, mimetype: photo.mimetype, size: photo.size})
+          })
+          writeProgress(progress);
+          res.send({
+              status: true,
+              message: 'successful upload',
+              id: id,
+              data: data,
+          });
+      }
+  } catch (err) {
+      res.status(500).send(err);
+  }
+  // const stylePyScript = spawn("python3 ../python/stylize.py", ["originalImage", "styleImage"])
 
-  // store in a local directory
+  // stylePyScript.stdout.on('data', data => {
+  //   progress.status = ERROR;
+  //   progress['data'] = data;
+  //   writeProgress(progress);
+  // })
 
-  // run the python script
-  const stylePyScript = spawn("python3 ../python/stylize.py", ["originalImage", "styleImage"])
+  // stylePyScript.stderr.on('data', err => {
 
-  stylePyScript.stdout.on('data', data => {
-    // do something
-  })
+  // })
 
-  stylePyScript.stderr.on('data', err => {
+  // stylePyScript.on('close', code => {
 
-  })
+  // })
 
-  stylePyScript.on('close', code => {
-
-  })
-
+  
   // send the URL of the finished image back
   res.send('finished image URL goes here');
 });
