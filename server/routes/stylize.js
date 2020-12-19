166a49b65b814001e089f81526cd7d313b921332
Larry Tseng <larry.tseng.1@gmail.com>
2020-12-18 23:23:16 -0500

    Convert vars to consts and create stylize endpoint

---
+++ server/routes/stylize.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { spawn } = require('child_process');
+
+const router = express.Router();
+
+/* GET stylize listing. */
+router.post('/', function(req, res, next) {
+  // grab image link from request body
+      // do we include raw images as well?
+
+  // preprocess the image
+
+  // store in a local directory
+
+  // run the python script
+  const stylePyScript = spawn("python3 ../python/stylize.py", ["originalImage", "styleImage"])
+
+  stylePyScript.stdout.on('data', data => {
+    // do something
+  })
+
+  stylePyScript.stderr.on('data', err => {
+
+  })
+
+  stylePyScript.on('close', code => {
+
+  })
+
+  // send the URL of the finished image back
+  res.send('finished image URL goes here');
+});
+
+module.exports = router;
