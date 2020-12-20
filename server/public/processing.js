ea68f01bc7e3ed2565ca786fe5144b5922b5a205
martin-dickie <cnoz0090@gmail.com>
2020-12-20 03:06:58 -0500

    not really processing, just holding some product stuff for now

---
+++ server/public/processing.js
@@ -0,0 +1,19 @@
+/*
+Processing.js
+this is a file used to mesh between prinfulapi and our usecase
+*/
+
+require('./printfulapi.js');
+
+
+/* products by id, in this order
+Men's Crew Neck.
+Woman's V-Neck
+Unisex Sweatshirt
+Men's Athetlic T-Shirt
+Woman's Athletic T-Shirt
+Unisex Hoodie
+Face Mask
+iPhone case
+*/
+let products = [257,260,320,328,329,388,434,181]
