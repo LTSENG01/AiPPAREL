aa54c3dadbd5d9e8dfaf9c35222ec0feb8401612
martin-dickie <cnoz0090@gmail.com>
2020-12-19 20:37:54 -0500

    upload.js now should work with stylize endpoint

---
+++ server/public/upload.js
@@ -0,0 +1,26 @@
+initialize();
+function initialize() {
+    console.log("test");
+    const submit_button = document.getElementById("submit_button");
+    submit_button.addEventListener("click", () => {
+        const formData = new FormData();
+        const photos = document.querySelector('input[type="file"][multiple]');
+
+        formData.append("images", "target_then_style");
+        for (let i = 0; i < photos.isDefaultNamespace.length; ++i) {
+            formData.append('images', photos.files[i])
+        }
+
+        fetch('https://aipparel.online/stylize', {
+            method: 'POST',
+            body: formData
+        })
+            .then(resposne => response.json())
+            .then(result => {
+                console.log('Success:', result);
+            })
+            .catch(error => {
+                console.error('Error', error);
+            });
+    });
+}
