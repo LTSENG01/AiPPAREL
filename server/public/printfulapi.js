221e2bce51f5d2d46cf7d7db9bec62962601c89a
martin-dickie <cnoz0090@gmail.com>
2020-12-20 01:33:36 -0500

    working on printfulapi

--- server/public/printfulapi.js
+++ server/public/printfulapi.js
@@ -1,40 +1,58 @@
-const { post } = require("../routes");
-
-let printful_api;
-if (!process.env.printful) {
-    printful_key = require('./secrets.json');
-    printful_api = printful_key.key;
-} else {
-    printful_api = process.env.printful;
-}
+//const { post } = require("../routes");
+const { printful_key } = require("../config.json");
+const { fs } = require('fs.js');
 
 async function getProduct(id) {
-    await fetch("https://api.printful.com/products/" + id).then(response => response.json()).then((prod) => { console.log(prod); }).catch((error) => { console.log("error" + error); });
+    await fetch("https://api.printful.com/products/" + id).then(response => response.json()).then((prod) => { fs.writeFile(hash+"_product_" + id + ".json",prod); }).catch((error) => { console.log("error" + error); });
 }
 
 async function makeTask(id, image) {
     let area_width_front, area_width_back, area_height_front, area_height_back,
         width_front, width_back, height_front, height_back, top_front, top_back, left_front, left_back, variants;
-
-
     await fetch('https://api.printful.com/mockup-generator/create-task/' + id, {
-        method: 'POST',
-        headers: {
-            authorization: printful_api
-        },
-        body: generateBody(id, image)
-    }).then(response => response.json()).then(result => { return result.task_key; });
+            method: 'POST',
+            headers: {
+                authorization: printful_key
+            },
+            body: generateBody(id, image)
+        }).then(response => response.json()).then(result => { return result.task_key; });
+    
 }
 
-async function getTask(task_key){
+async function getTask(task_key) {
     await fetch("https://api.printful.com/mockup-generator/task?task_key" + task_key).then(response => response.json()).then(result => {
-        if(result.status === "completed"){
-        console.log(result);
-        console.log(result.mockups[0].mockup_url);
-        }});
-        return 2;
+        if (result.status === "completed") {
+            console.log(result);
+            console.log(result.mockups[0].mockup_url);
+        }
+    });
+    return 2;
 }
 
+const default_position = {
+    "area_width": 1800,
+    "area_height": 2400,
+    "width": 1800,
+    "height": 1800,
+    "top": 300,
+    "left": 0
+}
+
+/*
+Generates a json body for use in creating tasks for printful API to assemble mockup images
+@input id - product id from printful
+@input image - image obtained through stylize
+@output body - json body used for task requests structure is:
+{
+    variant_ids: []
+    format: "jpg"
+    files: [{
+        placement:
+        image_url:
+        position: {}
+    }]
+}
+*/
 function generateBody(id, image) {
     let body = {};
     for (i = 0; i < products.length; i++) {
@@ -91,3 +109,18 @@ const products = [
     },
     { "name": "test", "id": "test1" }
 ];
+
+
+window.onload(initialize());
+
+function initialize(){
+    let contain = document.getElementById("container");
+    let butt = document.getElementById("submit_button");
+    butt.addEventListener("click",() => {
+       let task_id =  makeTask("257","https://cdn.discordapp.com/attachments/762812285608656907/790023342928822292/IMG_0261.jpeg");
+       let lab = document.createElement("label");
+        lab.innerText(task_id);
+        contain.appendChild(lab);
+    }
+    );
+}
